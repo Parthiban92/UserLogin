7324fabb728251b9a212b06bcd333845c8d0a401
Parthiban <parthiban.n@navoinc.com>
2020-08-06 19:03:55 +0530

    Updates

---
+++ precache-manifest.650c35ef164478f072114a8d0aebaebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3936b71668b34ad5714f6866820ff94f",
+    "url": "/UserLogin/index.html"
+  },
+  {
+    "revision": "afa9196def782351805a",
+    "url": "/UserLogin/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "af6fb00403a8be333aa9",
+    "url": "/UserLogin/static/css/main.f1a38a24.chunk.css"
+  },
+  {
+    "revision": "afa9196def782351805a",
+    "url": "/UserLogin/static/js/2.524896ea.chunk.js"
+  },
+  {
+    "revision": "dfcb393daa24fca698559682ae0c72f7",
+    "url": "/UserLogin/static/js/2.524896ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6fb00403a8be333aa9",
+    "url": "/UserLogin/static/js/main.d72e7c75.chunk.js"
+  },
+  {
+    "revision": "241d153e62789a4930c1",
+    "url": "/UserLogin/static/js/runtime-main.c945fb47.js"
+  }
+]);
